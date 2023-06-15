93c53dd787f1246e12e96fd2f443c18db945b9f1
dbfu <fudebao@byteplan.com>
2023-06-15 21:01:03 +0800

    feat:第一次启动项目自动创建数据库和初始化管理员账号

---
+++ script/init-database.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '12345678',
+});
+
+connection.connect(error => {
+  if (error) {
+    console.log('数据连接失败');
+    return;
+  }
+  connection.query(
+    "SELECT * FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'fluxy-admin'",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (result.length === 0) {
+        console.log('检测到数据库不存在，正在为你创建数据库...');
+        connection.query('CREATE DATABASE `fluxy-admin`');
+      }
+      connection.destroy();
+      process.exit();
+    }
+  );
+});
